bc5867ea7b2c1a3df307384ef7b8af5c3ac69fd9
BOB XUE <bob35xue@gmail.com>
2024-05-14 17:00:50 -0400

    'Python-Web-Scraping'

---
+++ pyWebScrap01.py
@@ -0,0 +1,93 @@
+# Python script to scrape an article given the url of the article and store the extracted text in a file
+# Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
+
+import os
+import sys
+import requests
+import re
+
+
+# Code here - Import BeautifulSoup library
+from bs4 import BeautifulSoup
+# soup = BeautifulSoup(res.content,'html5lib')  # If this line causes an error, run 'pip install html5lib' or install html5lib
+# Code ends here
+
+# function to get the html source text of the medium article
+def get_page():
+    global url
+
+    # Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
+
+    # Code ends here
+    url = "https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7"
+    # handling possible error
+    if not re.match(r'https?://medium.com/', url):
+        print('Please enter a valid website, or make sure it is a medium article')
+        sys.exit(1)
+
+    # Code here - Call get method in requests object, pass url and collect it in res
+
+    # response = requests.get(url)
+    headers = {'User-Agent': 'Your User Agent'}
+    res = requests.get(url, headers=headers)
+    # Code ends here
+
+    res.raise_for_status()
+    soup = "Error"
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.content, "html.parser")
+        print("Content Prettify:", soup.prettify())
+    else:
+        print(f"Error: {res.status_code}")
+
+    return soup
+
+
+# function to remove all the html tags and replace some with specific strings
+def clean(text):
+    rep = {"<br>": "\n", "<br/>": "\n", "<li>": "\n"}
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+    pattern = re.compile("|".join(rep.keys()))
+    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+    # text = re.sub('\<(.*?)\>', '', text)
+    return text
+
+
+def collect_text(soup):
+    text = f'url: {url}\n\n'
+    para_text = soup.find_all('p')
+    print(f"paragraphs text = \n {para_text}")
+    for para in para_text:
+        text += f"{para.text}\n\n"
+    return text
+
+
+# function to save file in the current directory
+def save_file(text):
+    root_dir = os.path.abspath(os.curdir)
+    print(root_dir)
+
+    folder_name = 'scraped_articles'
+    Scraped_Path = os.path.join(root_dir, folder_name)
+    print(Scraped_Path)
+
+    if not os.path.exists(Scraped_Path):
+        os.mkdir(Scraped_Path)
+    name = url.split("/")[-1]
+    print(name)
+    fname = f'scraped_articles/{name}.txt'
+
+    # Code here - write a file using with (2 lines)
+    with open(fname, "w") as f:
+        f.write(text)  # Write the input to the file
+
+    # Code ends here
+
+    print(f'File saved in directory {fname}')
+
+if __name__ == '__main__':
+    text = collect_text(get_page())
+
+    save_file(clean(text))
+# Instructions to Run this python code
+# Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
